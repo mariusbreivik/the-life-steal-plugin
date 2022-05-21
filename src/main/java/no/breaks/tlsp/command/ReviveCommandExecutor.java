2edc869c90a842376df4b4f3ad4ce70756c7ba44
Marius Breivik <marius.breivik@gmail.com>
2022-05-21 22:25:56 +0200

    added some tests

--- src/main/java/no/breaks/tlsp/command/ReviveCommandExecutor.java
+++ src/main/java/no/breaks/tlsp/command/ReviveCommandExecutor.java
@@ -1,16 +1,28 @@
 package no.breaks.tlsp.command;
 
+import no.breaks.tlsp.TheLifeStealPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
+import java.util.logging.Logger;
+
 public class ReviveCommandExecutor implements CommandExecutor {
+    private final TheLifeStealPlugin plugin;
+
+    private final Logger log;
+    public ReviveCommandExecutor(TheLifeStealPlugin plugin) {
+        this.plugin = plugin;
+        this.log = plugin.getLogger();
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
@@ -18,14 +30,26 @@ public class ReviveCommandExecutor implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "No player to revive selected (Ex: /lsp-revive PLAYER)");
             return false;
         }
-        Player killedPlayer = Bukkit.getPlayerExact(args[0]);
-        if(killedPlayer != null){
-            Location location = killedPlayer.getLocation();
-            killedPlayer.setGameMode(GameMode.SURVIVAL);
-            killedPlayer.teleport(location, PlayerTeleportEvent.TeleportCause.PLUGIN);
-            return true;
+        Player playerToBeRevived = Bukkit.getPlayerExact(args[0]);
+
+        if (playerToBeRevived == null) {
+            log.info("Player " + args[0] + " not found");
+            sender.sendMessage("Player " + args[0] + " not found");
+            return false;
         }
 
-        return false;
+        if (!playerToBeRevived.isDead()) {
+            log.info("Player " + args[0] + " not dead");
+            sender.sendMessage("Player " + args[0] + " not dead");
+            return false;
+        }
+
+        Location location = playerToBeRevived.getLocation();
+        playerToBeRevived.setGameMode(GameMode.SURVIVAL);
+        playerToBeRevived.teleport(location, PlayerTeleportEvent.TeleportCause.PLUGIN);
+        playerToBeRevived.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20);
+        log.info(sender.getName() + " revived " + playerToBeRevived.getDisplayName());
+        return true;
+
     }
 }
--- src/test/java/no/breaks/tlsp/listener/PlayerListenerTest.java
+++ src/test/java/no/breaks/tlsp/listener/PlayerListenerTest.java
@@ -2,10 +2,15 @@ package no.breaks.tlsp.listener;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.command.CommandResult;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
 import no.breaks.tlsp.TheLifeStealPlugin;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PlayerListenerTest {
 
@@ -24,7 +29,18 @@ class PlayerListenerTest {
     }
 
     @Test
-    void onPlayerKilled() {
+    void revivePlayerShouldResultInFailedCommandResultAndMessage() {
+        PlayerMock playerMock = server.addPlayer("MockPlayer");
+        CommandResult commandResult = server.executeConsole("lsp-revive", "MockPlayer");
+        commandResult.assertFailed();
+        commandResult.assertResponse("Player " + playerMock.getDisplayName() + " not dead");
+    }
 
+    @Test
+    void revivePlayerShouldResultInSucceededCommandResultAndMessage() {
+        PlayerMock killedPlayer = server.addPlayer("KilledPlayer");
+        PlayerMock killer = server.addPlayer("Killer");
+        killedPlayer.damage(500, killer);
+        assertTrue(killer.performCommand("lsp-revive KilledPlayer"));
     }
 }
